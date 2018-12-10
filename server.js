41857805d656ff931e58ca6feb184d174a57d201
Sullivan <githubST@gmail.com>
2018-12-10 13:50:15 +0100

    Request Insert test

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser');
 
 const server = express();
 
+/**
+ * Data Base Co
+ */
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/';
+
 server.use(bodyParser.urlencoded({extended: true}));
 server.listen(80);
 
@@ -11,6 +18,21 @@ server.get('/form.html', function(request, response) {
 });
 
 server.post('/post.html', function(request, response) {
-   const p1 = request.body.p1;
-   console.log(p1);
+   const name = request.body.name;
+   const address = request.body.address;
+
+   MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      const dbo = db.db('Form');
+      const myobj = {name: name, address: address};
+      dbo.collection('users').insertOne(myobj, function(err, res) {
+         if (err) throw err;
+         console.log('inserted');
+         db.close();
+      });
+   });
+   
 });
+
+
+
