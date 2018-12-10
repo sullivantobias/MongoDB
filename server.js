3c92e583600b9bafa90175f30808ba3b2043ac3b
Sullivan <githubST@gmail.com>
2018-12-10 17:17:58 +0100

    Test connection with user

--- server.js
+++ server.js
@@ -13,26 +13,44 @@ const url = 'mongodb://localhost:27017/';
 server.use(bodyParser.urlencoded({extended: true}));
 server.listen(80);
 
-server.get('/form.html', function(request, response) {
+server.get('/form.html', (request, response) => {
    response.sendFile( __dirname + '/form.html');
 });
 
-server.post('/post.html', function(request, response) {
+server.post('/signUp.html', (request, response) => {
    const name = request.body.name;
-   const address = request.body.address;
+   const pass = request.body.password;
 
-   MongoClient.connect(url, function(err, db) {
+   MongoClient.connect(url, (err, db) => {
       if (err) throw err;
       const dbo = db.db('Form');
-      const myobj = {name: name, address: address};
-      dbo.collection('users').insertOne(myobj, function(err, res) {
+      const user = {name: name, password: pass};
+      dbo.collection('users').insertOne(user, (err, res) => {
          if (err) throw err;
-         console.log('inserted');
+         response.sendFile( __dirname + '/form.html');
          db.close();
       });
    });
-   
 });
 
+server.post('/signIn.html', (request, response) => {
+   const nameIn = request.body.nameIn;
+   const passIn = request.body.passwordIn;
 
+   MongoClient.connect(url, (err, db) => {
+      if (err) throw err;
+      const dbo = db.db('Form');
+      const user = {name: nameIn, password: passIn};
 
+      dbo.collection('users').countDocuments(user, (err, res) => {
+         if (res > 0) response.sendFile( __dirname + '/signIn.html');
+         else {
+            response.sendFile( __dirname + '/form.html');
+         }
+
+         if (err) throw err;
+
+         db.close();
+      });
+   });
+});
